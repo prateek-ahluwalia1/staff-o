545d08dedfdd22dcbc82ea7ceaf82ccaefbc8310
Nauman <nauman33183@gmail.com>
2026-03-16 15:24:50 +0500

    search button color according to theme

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -425,9 +425,8 @@ const Invoice = () => {
             <div className="ms-auto">
               <button
                 type="button"
-                className="btn text-white px-4 py-2 shadow-sm d-flex align-items-center gap-2"
+                className="btn text-white px-4 py-2 shadow-sm d-flex align-items-center gap-2 bg-primary"
                 style={{
-                  backgroundColor: "#20c997",
                   borderRadius: "8px",
                   fontWeight: "600",
                 }}

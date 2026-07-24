cb4ef9103f66a0b7bc6f423e96c938cdd93a372c
Shahbaz Naeem <shahbazkhan062@gmail.com>
2026-07-24 16:08:24 +0500

    changes

--- app/Http/Controllers/Api/JobRosterController.php
+++ app/Http/Controllers/Api/JobRosterController.php
@@ -977,7 +977,7 @@ private function sendStaffActivationNotification(User $user): void
         {
             $a = null;
             $b = '';
-           return $roster = DB::table('job_rosters')
+            $roster = DB::table('job_rosters')
                 ->join('sites', 'sites.id', '=', 'job_rosters.site_id')
                 ->where('job_rosters.asap', '=', '1')
                 ->where('job_rosters.id', '=', $request->input('roster_id'))

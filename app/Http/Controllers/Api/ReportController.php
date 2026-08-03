6a550b688d29bdbf7122122324286f19824095b0
Shahbaz Naeem <shahbazkhan062@gmail.com>
2026-08-03 12:59:06 +0500

    changes

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -611,7 +611,7 @@ return $results;
                 'job_rosters.ph_night_hours',
                 'job_rosters.hours',
                 'job_rosters.site_id',
-                'sites.sitename as site_name',
+                'sites.site_name as site_name',
                 'job_rosters.accepted_by',
                 'contractors.name as contractor_name'
             )

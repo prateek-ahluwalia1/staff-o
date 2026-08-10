316286d4ad9e5641e3c6ad7edb2f40e07f21cc8d
Shahbaz Naeem <shahbazkhan062@gmail.com>
2026-08-10 13:02:39 +0500

    changes

--- app/Http/Controllers/Api/StripeWebhookController.php
+++ app/Http/Controllers/Api/StripeWebhookController.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+
 
 class StripeWebhookController extends Controller
 {

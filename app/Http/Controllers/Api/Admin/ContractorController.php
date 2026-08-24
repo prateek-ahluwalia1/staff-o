5449be5576389f1b56cce15384a1687aac7c9772
Shahbaz Naeem <shahbazkhan062@gmail.com>
2026-08-24 15:31:16 +0500

    changes

--- app/Http/Controllers/Api/Admin/ContractorController.php
+++ app/Http/Controllers/Api/Admin/ContractorController.php
@@ -215,9 +215,9 @@ private function calculateProfileCompletion(User $user): int
  
     $this->updateUserStatus($user, $newStatus);
 
-    if ($newStatus === 1 && $oldStatus != 1) {
+    if ($newStatus == 1 && $oldStatus != 1) {
         dispatch(new \App\Jobs\SendAccountStatusEmailJob($user, 'active'));
-    } elseif ($newStatus === 0 && $oldStatus != 0) {
+    } elseif ($newStatus == 0 && $oldStatus != 0) {
         dispatch(new \App\Jobs\SendAccountStatusEmailJob($user, 'inactive'));
     }
  

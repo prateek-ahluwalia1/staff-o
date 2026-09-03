b510100cf524ee70573fe952a3f13906886e51f3
Shahbaz Naeem <shahbazkhan062@gmail.com>
2026-09-03 11:32:04 +0500

    changes in invoice

--- app/Services/InvoiceService.php
+++ app/Services/InvoiceService.php
@@ -164,7 +164,7 @@ class InvoiceService
         $html .= "<tr><td class='lbl'>{$discountLabel}</td><td class='amt' style='color:#D97706;'>- {$discount}</td></tr>";
         $html .= "<tr class='grand'><td class='lbl'>Discounted Subtotal</td><td class='amt'>{$discountedsubtotal}</td></tr>";
         $html .= "<tr><td class='lbl'>GST (10%)</td><td class='amt'>{$serviceFee}</td></tr>";
-        $html .= "<tr class='grand'><td class='lbl'>Total</td><td class='amt'>{$total}</td></tr>";
+        // $html .= "<tr class='grand'><td class='lbl'>Total</td><td class='amt'>{$total}</td></tr>";
         $html .= "<tr class='charged'><td class='lbl'>Amount Charged Now</td><td class='amt'>{$amtCharged}</td></tr>";
         $html .= "<tr class='balance'><td class='lbl'>Due</td><td class='amt'>{$balance}</td></tr>";
         $html .= "</table></div>";

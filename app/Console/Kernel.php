44ee5654622067e41a2e22afc8f5cf30bfe7b67b
Shahbaz Naeem <shahbazkhan062@gmail.com>
2026-09-03 15:52:22 +0500

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,10 +27,6 @@ class Kernel extends ConsoleKernel
         ->everyMinute()
         ->withoutOverlapping();
         $schedule->command('jobs:send-payment-reminders')->everyFifteenMinutes();
-        // $schedule->command('timesheet:send-weekly')
-        //     ->weekly()
-        //     ->mondays()
-        //     ->at('08:00');
          // $schedule->command('app:sync-public-holidays')->twiceYearly(1, 1);
 
 

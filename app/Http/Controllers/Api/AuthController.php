d497b8cbc37377ec0af69dac41408f4591c7dd07
Shahbaz Naeem <shahbazkhan062@gmail.com>
2026-09-16 10:10:51 +0500

    changes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -175,8 +175,7 @@ class AuthController extends Controller
         }
 
         $phone = $this->formatPhone($phone);
-        // $message = "{$otp} is your Staffoo OTP. Enter the code on Staffoo to verify your phone number.";
-        $message = "sms_2fa";
+        $message = "{$otp} is your Staffoo OTP. Enter the code on Staffoo to verify your phone number.";
         $sendSmS = send_sms($phone, $message);
 
         return $sendSmS;
